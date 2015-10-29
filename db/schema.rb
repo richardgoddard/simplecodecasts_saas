14b0112e8937c7c95b5c590a4dd70de8abca1a00
Richard Goddard <richg1991@gmail.com>
2015-10-29 21:43:18 +0000

    Added profile model and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028232155) do
+ActiveRecord::Schema.define(version: 20151029214010) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20151028232155) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "profiles", force: true do |t|
   end
 
   create_table "users", force: true do |t|
